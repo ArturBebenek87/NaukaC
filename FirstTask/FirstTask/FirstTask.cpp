1ff7fcd03fb47894a3fcc8917bd1ea8169a4147e
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-02 21:33:31 +0200

    Add files via upload

---
+++ FirstTask/FirstTask/FirstTask.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl"); //możliwość wyświetlania polskich znaków
+
+	int x = 5; //przypisanie wartości do zmiennej x
+	int y = 7; //przypisanie wartości do zmiennej y
+
+	cout << "Zmienna x ma wartość: " << x << endl;
+	cout << "Zmienna y ma wartość: " << y << endl;
+
+	cout << endl << x + y << " " << x * y << endl; //wyświetlenie wyniku dodawania i mnożenia zmiennych x i y
+
+	return 0;
+}
+
