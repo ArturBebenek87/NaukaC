3ee7fefd6fcb6911a1b2e4fa1e4f082a6fb2274c
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-03 22:16:01 +0200

    Add files via upload

---
+++ FirstFunction/FirstFunction/FirstFunction.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//pierwsza funkcja
+
+void DemonstrationFunction()
+{
+	cout << "Wewnątrz funkcji DemonstrationFunction." << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	cout << "Wewnątrz funkcji main." << endl;
+	DemonstrationFunction(); //wywołanie funkcji DemonstrationFunction
+	cout << "Ponownie w funkcji main." << endl;
+
+	return 0;
+}
