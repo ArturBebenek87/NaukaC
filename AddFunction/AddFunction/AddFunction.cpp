b7ef3cb6794ffffcbb8f85006b4eb4284e76b2cc
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-03 22:16:28 +0200

    Add files via upload

---
+++ AddFunction/AddFunction/AddFunction.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Add(int first, int second)
+{
+	cout << "Funkcja Add() otrzymała dwie wartości: " << first << " oraz " << second << endl;
+
+	return (first + second);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	cout << "Jestem w funkcji main()!" << endl;
+	int a, b, c;
+	cout << "Wpisz dwie liczby: ";
+	cin >> a;
+	cin >> b;
+	cout << endl << "Wywołanie funkcji Add()" << endl;
+	c = Add(a, b);
+	cout << "Ponownie w funkcji main()." << endl;
+	cout << "c zostało ustawione na " << c << endl;
+	cout << "Opuszczenie programu" << endl;
+
+	return 0;
+}
