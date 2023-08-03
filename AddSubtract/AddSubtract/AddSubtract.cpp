bf2595733b7ae0355c545b73e5ede58cb0261519
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-03 22:16:56 +0200

    Add files via upload

---
+++ AddSubtract/AddSubtract/AddSubtract.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//funkcja odpowiedzialna za dodawanie
+
+int Add(int first, int second)
+{
+	cout << "Funkcja Add() otrzymała wartości: " << first << " oraz " << second << endl;
+
+	return (first + second);
+}
+
+//funkcja odpowiedzialna za odejmowanie
+
+int Subtract(int first, int second)
+{
+	cout << "Funkcja Subtract() otrzymała wartość: " << first << " oraz " << second << endl;
+
+	return (first - second);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	cout << "Wewnątrz funkcji main()." << endl;
+
+	int a, b, c;
+
+	cout << "Podaj dwie liczby: ";
+
+	cin >> a;
+	cin >> b;
+
+	cout << endl << "Wywołanie funkcji Add()." << endl;
+	c = Add(a, b);
+	cout << "Wynikiem dodawania podanych liczb jest: " << c << endl;
+
+	cout << endl << "Wywołanie funkcji Subtract()." << endl;
+	c = Subtract(a, b);
+	cout << "Wynikiem odejmowania podanych liczb jest: " << c << endl;
+
+	return 0;
+
+}
