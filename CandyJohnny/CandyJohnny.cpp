605c8aa7d0bb9ef0555ce81e615dbdc892e91bf7
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-06 19:05:32 +0200

    Add files via upload

---
+++ CandyJohnny/CandyJohnny.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int uczniowie;
+int cukierki;
+int x;
+int y;
+
+int main()
+{
+	setlocale (LC_ALL, "PL_pl");
+
+	cout << "Ile uczniów liczy klasa: ";
+	cin >> uczniowie;
+
+	cout << "Ile sztuk cukierków zostało kupionych: ";
+	cin >> cukierki;
+
+	x = cukierki / (uczniowie - 1);
+	cout << endl << "Ile cukierków dla każdego ucznia: " << x << endl;
+
+	y = cukierki - x * (uczniowie - 1);
+	cout << "Ilość cukierków, które zostały dla solenizanta: " << y << endl;
+
+	return 0;
+}
