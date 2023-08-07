fb4ed702ef0b489ffc0395d79df0b9b96d6dacc9
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-07 22:10:28 +0200

    Add files via upload

---
+++ sizeVeriable/sizeVeriable/sizeVeriable.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	cout << "Rozmiar zmiennej typu int to:\t\t" << sizeof(int) << " bajty." << endl;
+	cout << "Rozmiar zmiennej typu short int to:\t" << sizeof(short) << " bajty." << endl;
+	cout << "Rozmiar zmiennej typu long int to:\t" << sizeof(long) << " bajty." << endl;
+	cout << "Rozmiar zmienej typu char to:\t\t" << sizeof(char) << " bajt." << endl;
+	cout << "Rozmiar zmiennej typu float to:\t\t" << sizeof(float) << " bajty." << endl;
+	cout << "Rozmiar zmiennej typu double to:\t" << sizeof(double) << " bajtów." << endl;
+	cout << "Rozmiar zmiennej typu bool to:\t\t" << sizeof(bool) << " bajt." << endl;
+
+	return 0;
+}
