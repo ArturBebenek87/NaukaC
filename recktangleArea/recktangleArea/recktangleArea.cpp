071fe6be4f8465160bef27d95e6780c73713fb80
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-07 22:10:58 +0200

    Add files via upload

---
+++ recktangleArea/recktangleArea/recktangleArea.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int Width = 5, Length = 10;
+	int Area = Width * Length;
+
+	cout << "Szerokość prostokąta wynosi: " << Width << endl;
+	cout << "Długość prostokąta wynosi: " << Length << endl;
+
+	cout << endl << "Obszar prostokąta wynosi: " << Area << endl;
+
+	return 0;
+}
