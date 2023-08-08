89ad28d32993cdbf969aec5da4486dd729d56e7e
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-08 20:31:49 +0200

    Add files via upload

---
+++ firstArray/firstArray/firstArray.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int myArray[5]; //deklaracja 5 liczb całkowitych
+	int i;
+	for (i = 0; i < 5; i++)
+	{
+		cout << "Wartość elementu myArray[" << i << "]: ";
+		cin >> myArray[i];
+	}
+	for (i = 0; i < 5; i++)
+		cout << i << ": " << myArray[i] << endl;
+	
+	return 0;
+}
