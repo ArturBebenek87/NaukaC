e086c07938e2bb608855dce29c3b8db2e722cfef
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-08 20:30:52 +0200

    Add files via upload

---
+++ constDays/constDays/constDays.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	enum Days //deklaracja stałej wyliczeniowej
+	{
+		Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+	};
+
+	Days today;
+	today = Wednesday;
+
+	if (today == Sunday || today == Saturday)
+		cout << "Weekendy są super!" << endl;
+	else
+		cout << "Niestety dzisiaj jeszcze nie ma weekendu." << endl;
+
+	return 0;
+}
