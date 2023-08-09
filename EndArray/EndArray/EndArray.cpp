09d06ec72dd61712cfbdcb06746b7fff0fdda522
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-09 20:01:16 +0200

    Add files via upload

---
+++ EndArray/EndArray/EndArray.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int FirstArray[5] = { 12, 13, 14, 23, 34 };
+
+	cout << "Pierwsza tablica składa się z 5 elementów." << endl;
+
+	cout << endl << "Pierwszy element tablicy to:\t" << FirstArray[0] << endl;
+	cout << "Drugi element tablicy to:\t" << FirstArray[1] << endl;
+
+	return 0;
+}
