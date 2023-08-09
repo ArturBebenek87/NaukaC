6e8c53db7271af510725d3c1d032eabbf79d5a4e
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-09 20:01:44 +0200

    Add files via upload

---
+++ arrayWeek/arrayWeek/arrayWeek.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	//deklaracja typu wyliczeniowego
+	enum  dniTygodnia
+	{
+		Mon, Tue, Wed, Thu, Fri, Sat, Sun, DniWTygodniu
+	};
+
+	//Deklaracja tablicy
+	int ArrayWeek[DniWTygodniu] = { 10, 20, 30, 40, 50, 60, 70 };
+
+	cout << "Wartością piątku w tablicy jest: " << ArrayWeek[Fri] << endl;
+
+	return 0;
+}
