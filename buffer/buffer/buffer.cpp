844b6104124578086a0b5e1d4a313e711a021f61
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-11 23:26:36 +0200

    Add files via upload

---
+++ buffer/buffer/buffer.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	char buffer[80] = { '\0' };
+	cout << "Wpisz ciąg tekstowy: ";
+	cin >> buffer;
+	cout << "Oto zawartość bufora, którą wprowadziłeś: " << buffer << endl;
+
+	return 0;
+}
