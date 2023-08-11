ee369bd5c427bc569d710cdb6e9aece09235df21
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-11 23:31:11 +0200

    Add files via upload

---
+++ bufferModify/bufferModify/bufferModify.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	char buffer[80] = { '\0' };
+	cout << "Wpisz ciąg znaków: ";
+	cin.get(buffer, 79);
+	cout << "Podany przez Ciebie ciąg znaków to: " << buffer << endl;
+
+	return 0;
+}
