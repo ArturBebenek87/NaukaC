c0148e50922f99692a37396fa956efb747206f8b
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-11 23:43:29 +0200

    Add files via upload

---
+++ copyString/copyString/copyString.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	char String1[] = "Tekst, który będzie skopiowany";
+	char String2[80] = { '\0' };
+
+	strcpy_s(String2, String1);
+
+	cout << "String1: " << String1 << endl;
+	cout << "String2: " << String2 << endl;
+
+	return 0;
+}
