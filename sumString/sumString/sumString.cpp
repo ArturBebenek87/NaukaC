39d33b18c93aa5e2ec6deaea272d2b2b0eb0d7a3
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-13 10:22:57 +0200

    Add files via upload

---
+++ sumString/sumString/sumString.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	string str1("Ciąg tekstowy w C++! ");//przypisanie ciągu tekstowego do str1
+
+	cout << "str1 = " << str1 << endl;
+	
+	string str2;
+
+	str2 = str1;
+
+	cout << "Wynik przypisania str2 = " << str2 << endl;
+
+	str2 = "Witaj nowy ciągu tekstowy!";
+
+	cout << "Po nadpisaniu treści str2 = " << str2 << endl << endl;
+
+	string strAddResult;
+
+	strAddResult = str1 + str2;
+
+	cout << "Wynik dodawania wartości str1 i str2 to: " << strAddResult << endl;
+
+	return 0;
+}
