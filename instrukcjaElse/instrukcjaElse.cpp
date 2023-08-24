9a2314096270233864a5751cfa30bb68a6237ffa
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-24 08:31:00 +0200

    Add files via upload

---
+++ instrukcjaElse/instrukcjaElse.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int firstNumber, secondNumber;
+
+	cout << "Podaj większą liczbę: ";
+	cin >> firstNumber;
+	cout << "Podaj mniejszą liczbę: ";
+	cin >> secondNumber;
+
+	cout << endl;
+
+	if (firstNumber > secondNumber)
+		cout << "Dziękuję, podano prawidłowe liczby." << endl;
+	else
+		cout << "Nastąpiła pomyłka. Druga podana liczba jest większa od pierwszej!" << endl;
+
+	return 0;
+}
