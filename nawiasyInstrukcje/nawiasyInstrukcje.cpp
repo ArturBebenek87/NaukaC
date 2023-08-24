d7694456ead803bd8ba32253f2336431dcb83aae
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-24 08:31:52 +0200

    Add files via upload

---
+++ nawiasyInstrukcje/nawiasyInstrukcje.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int x;
+
+	cout << "Wpisza liczbę mniejszą niż 10 lub większą niż 100: ";
+	cin >> x;
+	cout << endl;
+
+	if (x >= 10)
+	{
+		if (x > 100)
+			cout << "Większa niż 100. Dziękuję!" << endl;
+	}
+	else
+		cout << "Mniejsze niż 10. Dziękuję!" << endl;
+
+	return 0;
+}
