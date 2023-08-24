68e60796146f2ab420da941a314515f3b35ba29b
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-24 08:31:27 +0200

    Add files via upload

---
+++ zagniezdzanieInstrukcji/zagniezdzanieInstrukcji.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int firstNumber, secondNumber;
+
+	cout << "Wpisz dwie liczby." << endl;
+	cout << endl << "Pierwsza liczba: ";
+	cin >> firstNumber;
+
+	cout << "Druga liczba: ";
+	cin >> secondNumber;
+
+	if (firstNumber >= secondNumber)
+	{
+		if ((firstNumber % secondNumber) == 0) //dzielenie bez reszty
+		{
+			if (firstNumber == secondNumber)
+				cout << "Obie podane liczby są takie same." << endl;
+			else
+				cout << "Podane liczby dzielą się bez reszty." << endl;
+		}
+		else
+			cout << "Podane liczby nie dzielą się bez reszty." << endl;
+	}
+	else
+		cout << "Druga podana liczba jest większa!" << endl;
+	
+	return 0;
+}
