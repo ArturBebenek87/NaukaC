e45a7a2cca8c75b1c1220c04e9fb663cdcd39160
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-25 10:52:01 +0200

    Add files via upload

---
+++ zadanie2/zadanie2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	using namespace std;
+
+	int a = 2, b = 2, c;
+	
+	if (c = (a - b))
+		cout << "Wartość c wynosi: " << c << endl;
+
+	return 0;
+}
