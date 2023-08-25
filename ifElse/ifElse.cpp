e328ac07cc8969e9220fbd6b55a466cf03881cf1
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-25 10:51:30 +0200

    Add files via upload

---
+++ ifElse/ifElse.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int x, y;
+
+	cout << "Wprowadź dwie liczby. Pierwsza liczba: ";
+	cin >> x;
+	cout << endl << "Wprowadź drugą liczbę: ";
+	cin >> y;
+	cout << endl;
+
+	if (x > y) //jeśli x jest większe niż y wartość x zostanie przypisana y
+		x = y;
+	else
+		y = x;
+
+	cout << "Jeśli pierwsza liczba jest większa niż druga, to nowa wartość drugiej liczby to: " << y << endl;
+
+	return 0;
+}
