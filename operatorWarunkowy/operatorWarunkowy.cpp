72a4af70836a630d1a92c528f811294af8cdc24d
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-25 10:51:05 +0200

    Add files via upload

---
+++ operatorWarunkowy/operatorWarunkowy.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int x, y, z;
+
+	cout << "Wpisz dwie liczby." << endl;
+
+	cout << "Pierwsza: ";
+	cin >> x;
+
+	cout << "Druga: ";
+	cin >> y;
+
+	cout << endl;
+
+	if (x > y)
+	{
+		z = x;
+	}
+	else
+	{
+		z = y;
+	}
+
+	cout << "Po sprawdzeniu if, z: " << z << endl;
+
+	z = (x > y) ? x : y;
+
+	cout << "Po sprawdzeniu warunku, z: " << z << endl;
+
+	return 0;
+}
