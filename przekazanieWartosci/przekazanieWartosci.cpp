967888c63ffe30008945e713055761388fc8c371
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-26 15:19:43 +0200

    Add files via upload

---
+++ przekazanieWartosci/przekazanieWartosci.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void swap(int x, int y);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_Pl");
+
+	int x = 5, y = 10;
+
+	cout << "Funkcja Main. Przed funkcją Swap, x: " << x << " y: " << y << endl;
+
+	swap(x, y);
+
+	cout << "Funkcja Main. Po funkcji Swap, x: " << x << " y: " << y << endl;
+
+	return 0;
+}
+
+void swap(int x, int y)
+{
+	int temp;
+
+	cout << "Funkcja Swap. Przed zmianą, x: " << x << " y: " << y << endl;
+
+	temp = x;
+	x = y;
+	y = temp;
+
+	cout << "Funkcja Swap. Po zmianie, x: " << x << " y: " << y << endl;
+}
