17e958fce613438a87be083a64c228815b820b85
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-26 15:18:48 +0200

    Add files via upload

---
+++ zmienneLokalne/zmienneLokalne.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float Convert(float);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	float tempFer;
+	float tempCel;
+
+	cout << "Podaj temperaturę w stopniach Fahrenheita: ";
+	cin >> tempFer;
+	tempCel = Convert(tempFer);
+
+	cout << "Odpowiadająca jej temperatura w stopniach Celsjusza: " << tempCel << endl;
+
+	return 0;
+}
+
+float Convert(float tempFer)
+{
+	float tempCel;
+	tempCel = ((tempFer - 32) * 5) / 9;
+	return tempCel;
+}
