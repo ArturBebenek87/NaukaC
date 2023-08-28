80d56b53c4b2b533b562d49a60d959464814706c
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-28 22:28:54 +0200

    Add files via upload

---
+++ paramDefault/paramDefault.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int AreaCube(int length, int width = 25, int height = 1);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int length = 100;
+	int width = 50;
+	int height = 2;
+	int area;
+
+	area = AreaCube(length, width, height);
+
+	cout << "Za pierwszym razem objętość wynosi: " << area << endl;
+
+	area = AreaCube(length, width);
+
+	cout << "Za drugim razem objętość wynosi: " << area << endl;
+
+	area = AreaCube(length);
+
+	cout << "Za trzecim razem objętość wynosi: " << area << endl;
+
+	return 0;
+}
+
+int AreaCube(int length, int width, int height)
+{
+	return (length * width * height);
+}
