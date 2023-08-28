0024bf4b5cbb82fe00c0bd86b18163bcff84b205
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-28 22:27:59 +0200

    Add files via upload

---
+++ zmienneGlobalne/zmienneGlobalne.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void myFunction();
+
+int x = 5, y = 7;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	cout << "x z funkcji main: " << x << endl;
+	cout << "y z funkcji main: " << y << endl << endl;
+
+	myFunction();
+
+	cout << "Powrót z funkcji myFunction!" << endl << endl;
+	cout << "x z funkcji main: " << x << endl;
+	cout << "y z funkcji main: " << y << endl;
+
+	return 0;
+}
+
+void myFunction()
+{
+	int y = 10;
+
+	cout << "x z funkcji myFunction: " << x << endl;
+	cout << "y z funkcji myFunction: " << y << endl << endl;
+}
