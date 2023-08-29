ee35068264d84dcab8df11e9bf5772f13c576759
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-29 15:24:54 +0200

    Add files via upload

---
+++ inlineFunction/inlineFunction.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+inline int Double(int);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int target;
+
+	cout << "Podaj liczbę: ";
+	cin >> target;
+	cout << endl;
+
+	target = Double(target);
+	cout << "Wynik: " << target << endl;
+
+	target = Double(target);
+	cout << "Wynik: " << target << endl;
+
+	target = Double(target);
+	cout << "Wynik: " << target << endl;
+
+	return 0;
+}
+
+int Double(int target)
+{
+	return 2 * target;
+}
