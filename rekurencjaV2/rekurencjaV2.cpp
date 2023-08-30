f4c87113330b7eba35a4ae7a3ab666b33336821e
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-30 20:07:13 +0200

    Add files via upload

---
+++ rekurencjaV2/rekurencjaV2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef unsigned short USHORT;
+typedef unsigned long ULONG;
+
+ULONG GetPower(USHORT n, USHORT power);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	USHORT number, power;
+	ULONG answer;
+
+	cout << "Podaj liczbę: ";
+	cin >> number;
+	cout << "Do której potęgi? ";
+	cin >> power;
+
+	cout << endl;
+
+	answer = GetPower(number, power);
+
+	cout << number << " do potęgi " << power << " wynosi " << answer << endl;
+
+	return 0;
+}
+
+ULONG GetPower(USHORT n, USHORT power)
+{
+	if (power == 1)
+		return n;
+	else
+		return (n * GetPower(n, power - 1));
+}
