2c53ed4d3df1610fcc30c6f4840f575cc41e2363
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-30 20:06:47 +0200

    Add files via upload

---
+++ divader/divader.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+short int Divider(unsigned short int valone, unsigned short int valtwo);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	unsigned short int one, two;
+	short int answer;
+
+	cout << "Podaj dwie liczby." << endl << "Pierwsza liczba: ";
+	cin >> one;
+	cout << "Druga liczba: ";
+	cin >> two;
+
+	answer = Divider(one, two);
+
+	if (answer > -1)
+		cout << "Odpowiedź: " << answer;
+	else
+		cout << "Błąd! Nie można dzielić przez zero!";
+	
+	return 0;
+}
+
+short int Divider(unsigned short int valOne, unsigned short int valTwo)
+{
+	if (valTwo == 0)
+		return -1;
+	else
+		return valOne / valTwo;
+}
