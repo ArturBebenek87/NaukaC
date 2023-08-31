737fbe072521ce172bd8287aa52f4acfa7254364
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-31 19:29:50 +0200

    Add files via upload

---
+++ petlaWhileV3/petlaWhileV3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	unsigned short small;
+	unsigned long large;
+	const unsigned short MAXSMALL = 65535;
+
+	cout << "Wpisz mniejszą liczbę: ";
+	cin >> small;
+	cout << "Wpisz dużą liczbę: ";
+	cin >> large;
+	cout << "mała: " << small << "...";
+
+	//sprawdzanie dwóch warunków
+	while (small < large && small < MAXSMALL)
+	{
+		if (small % 5000 == 0) //wyświetlanie kropki co każde 5000
+			cout << ".";
+
+		small++;
+		large -= 2;
+	}
+
+	cout << endl << "Mała: " << small << " Duża: " << large << endl;
+
+	return 0;
+}
