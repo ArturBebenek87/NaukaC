e2cea3049345ba8c1434db6011209d5b5f885eef
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-31 19:29:23 +0200

    Add files via upload

---
+++ petlaWhileV2/petlaWhileV2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int x = 0;
+
+	while (x < 10)
+	{
+		cout << "X = " << x++ << endl;
+	}
+
+	cout << endl << "Koniec instrukcji. " << x << endl;
+
+	return 0;
+}
