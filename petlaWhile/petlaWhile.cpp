a0cff8e78a7e2b0b01f6c087c4af0e0fab5553b2
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-31 19:28:56 +0200

    Add files via upload

---
+++ petlaWhile/petlaWhile.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter = 0;
+
+	while (counter < 5) //sprawdzenie warunku
+	{
+		counter++;
+		cout << "Licznik: " << counter << endl;
+	}
+
+	cout << "Gotowe. Licznik: " << counter << endl;
+
+	return 0;
+}
