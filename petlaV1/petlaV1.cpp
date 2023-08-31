377c01ba540902ad5e2ac6e8ecb068a3e6539dac
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-31 19:28:27 +0200

    Add files via upload

---
+++ petlaV1/petlaV1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter = 0;
+
+loop:
+
+	counter++;
+	cout << "Licznik: " << counter << endl;
+
+	if (counter < 5)
+		goto loop;
+
+	cout << "Gotowe. Licznik: " << counter << endl;
+	return 0;
+}
