6823644683014be5d5090e364365daab94ef85d3
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-08-31 19:30:16 +0200

    Add files via upload

---
+++ instrukcjeContinue/instrukcjeContinue.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	unsigned short small;
+	unsigned long large;
+	unsigned long skip;
+	unsigned long target;
+	const unsigned short MAXSMALL = 65535;
+
+	cout << "Wpisz mniejszą liczbę: ";
+	cin >> small;
+	cout << "Wpisz większą liczbę: ";
+	cin >> large;
+	cout << "Wpisz liczbę pomijaną: ";
+	cin >> skip;
+	cout << "Wpisz liczbę docelową: ";
+	cin >> target;
+
+	cout << endl;
+
+	while (small < large && small < MAXSMALL)
+	{
+		small++;
+
+		if (small % skip == 0)
+		{
+			cout << "Pominięto dla " << small << endl;
+			continue;
+		}
+
+		if (large == target)
+		{
+			cout << "Osiągnięto wartość docelową!";
+			break;
+		}
+
+		large -= 2;
+	}
+
+	cout << endl << "Mniejsza: " << small << " Większa: " << large << endl;
+	return 0;
+}
