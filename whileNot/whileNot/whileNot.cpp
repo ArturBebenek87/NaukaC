7234450d17a21ebccc440aa6f815fe8db43648db
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-04 21:29:37 +0200

    Add files via upload

---
+++ whileNot/whileNot/whileNot.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter;
+
+	cout << "Ile pozdrowień?: ";
+	cin >> counter;
+
+	while (counter > 0)
+	{
+		cout << "Cześć!" << endl;
+		counter--;
+	}
+
+	cout << "Wartość licznika: " << counter << endl;
+	return 0;
+}
