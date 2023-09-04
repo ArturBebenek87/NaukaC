a6f77dc9bd22d7407fc83b76387fb7d47fb12902
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-04 21:31:06 +0200

    Add files via upload

---
+++ whileV4/whileV4/whileV4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter = 0;
+
+	while (counter < 5)
+	{
+		counter++;
+		cout << "Pętla! ";
+	}
+
+	cout << endl << "Licznik: " << counter << endl;
+	return 0;
+}
