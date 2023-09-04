3df5a91f36481c2dcf7426a68d5107584c80af73
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-04 21:31:47 +0200

    Add files via upload

---
+++ forLoop/forLoop/forLoop.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter;
+
+	for (counter = 0; counter < 5; counter++)
+		cout << "Pętla! ";
+
+	cout << endl << "Licznik: " << counter << endl;
+	return 0;
+}
