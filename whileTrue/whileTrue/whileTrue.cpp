17c0a74a8acf53d7bdc9bc1f239215c1532a2576
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-04 21:29:06 +0200

    Add files via upload

---
+++ whileTrue/whileTrue/whileTrue.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter = 0;
+
+	while (true)
+	{
+		counter++;
+		if (counter > 10)
+			break;
+	}
+
+	cout << "Licznik: " << counter << endl;
+	return 0;
+}
