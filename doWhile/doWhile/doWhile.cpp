68c17cbb50a9f9a9c81c0b22da96667370538f7c
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-04 21:30:05 +0200

    Add files via upload

---
+++ doWhile/doWhile/doWhile.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter;
+
+	cout << "Ile pozdrowień? ";
+	cin >> counter;
+
+	do
+	{
+		cout << "Cześć!" << endl;
+		counter--;
+	} while (counter > 0);
+	cout << "Licznik ma wartość: " << counter << endl;
+	return 0;
+}
