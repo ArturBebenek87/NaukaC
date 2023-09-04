65b6441169b25b34cda9451a5702c207b2d990a4
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-04 21:32:15 +0200

    Add files via upload

---
+++ forLoopV2/forLoopV2/forLoopV2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	for (int i = 0, j = 0; i < 3; i++, j++)
+		cout << "i: " << i << " j: " << j << endl;
+
+	return 0;
+}
