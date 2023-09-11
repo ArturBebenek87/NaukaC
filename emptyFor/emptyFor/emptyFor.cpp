61140b945605a3c21918f106c794839032783c50
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-11 20:28:00 +0200

    Add files via upload

---
+++ emptyFor/emptyFor/emptyFor.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter = 0;
+
+	for (; counter < 5; )
+	{
+		counter++;
+		cout << "Pętla! ";
+	}
+
+	cout << endl << "Licznik: " << counter << endl;
+	return 0;
+}
