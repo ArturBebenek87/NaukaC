c5f8439739ca00ee8177ce597b902e0bca4fd95f
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-11 20:30:25 +0200

    Add files via upload

---
+++ zakresZmiennychFor/zakresZmiennychFor/zakresZmiennychFor.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		cout << "i: " << i << endl;
+	}
+	i = 7; //i znajdujące sie w zakresie każdego kompilatora
+	return 0;
+}
