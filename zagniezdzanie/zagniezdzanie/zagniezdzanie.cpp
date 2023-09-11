67606d800d7b1aa79cb3eba9cc8995dc13135134
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-11 20:29:56 +0200

    Add files via upload

---
+++ zagniezdzanie/zagniezdzanie/zagniezdzanie.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int rows, columns;
+	char theChar;
+
+	cout << "Ile wierszy? ";
+	cin >> rows;
+	cout << "Ile kolumn? ";
+	cin >> columns;
+	cout << "Jaki znak? ";
+	cin >> theChar;
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+			cout << theChar;
+		cout << endl;
+	}
+	return 0;
+}
