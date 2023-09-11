1d9e9b7b30fe7b12646628f85cb0df324a24ce5b
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-11 20:28:34 +0200

    Add files via upload

---
+++ forPustyNaglowek/forPustyNaglowek/forPustyNaglowek.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int counter = 0;
+	int max;
+
+	cout << "Ile pozdrowień? ";
+	cin >> max;
+
+	for (;;) //niekończąca się pętla
+	{
+		if (counter < max)
+		{
+			cout << "Cześć! " << endl;
+			counter++;
+		}
+		else
+			break;
+	}
+	return 0;
+}
