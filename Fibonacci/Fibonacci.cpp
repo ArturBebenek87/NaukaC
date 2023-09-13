47114f5a2514d8053b8e380da30321beed18e9d5
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-13 22:37:14 +0200

    Add files via upload

---
+++ Fibonacci/Fibonacci.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+unsigned int fib(unsigned int position);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	unsigned int answer, position;
+
+	cout << "Który wyraz ciągu? ";
+	cin >> position;
+
+	cout << endl;
+
+	answer = fib(position);
+	cout << position << " wyraz ciągu Fibonacciego ";
+	cout << "ma wartość " << answer << endl;
+	return 0;
+}
+
+unsigned int fib(unsigned int n)
+{
+	unsigned int minusTwo = 1, minusOne = 1, answer = 2;
+
+	if (n < 3)
+		return 1;
+
+	for (n -= 3; n != 0; n--)
+	{
+		minusTwo = minusOne;
+		minusOne = answer;
+		answer = minusOne + minusTwo;
+	}
+	return answer;
+}
