d1ee50a668fa560fad7623979348af275defe7ae
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-14 22:15:54 +0200

    Add files via upload

---
+++ operatorAdresu/operatorAdresu/operatorAdresu.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	unsigned short shortVar = 5;
+	unsigned long longVar = 65535;
+	long sVar = -65535;
+
+	cout << "shortVar:\t" << shortVar;
+	cout << "\tAdres zmiennej shortVar:\t";
+	cout <<  &shortVar << endl;
+
+	cout << "longVar:\t" << longVar;
+	cout << "\tAdres zmiennej longVar:\t";
+	cout << &longVar << endl;
+
+	cout << "sVar:\t\t" << sVar;
+	cout << "\tAdres zmiennej sVar:\t";
+	cout << &sVar << endl;
+
+	return 0;
+}
