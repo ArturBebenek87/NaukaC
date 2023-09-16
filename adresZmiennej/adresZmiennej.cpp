7e7d1ca2c8cd418d8f472d25ac404e7c0c8d1ef6
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-16 21:04:20 +0200

    Add files via upload

---
+++ adresZmiennej/adresZmiennej.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+typedef unsigned short int USHORT;
+
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	USHORT myAge; //zmienna
+	USHORT* pAge = 0; //wskaźnik
+
+	myAge = 5;
+
+	cout << "myAge: " << myAge << endl;
+	pAge = &myAge;
+	cout << "*pAge: " << *pAge << endl << endl;
+
+	cout << "Ustawiam *pAge = 7..." << endl;
+	*pAge = 7;
+
+	cout << "*pAge: " << *pAge << endl;
+	cout << "myAge: " << myAge << endl << endl;
+
+	cout << "Ustawiam myAge = 9..." << endl;
+	myAge = 9;
+
+	cout << "myAge: " << myAge << endl;
+	cout << "*pAge: " << *pAge << endl;
+
+	return 0;
+}
