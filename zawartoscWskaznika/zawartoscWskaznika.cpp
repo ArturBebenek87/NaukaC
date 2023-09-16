cba3b3181628f0d91e6fce77b4289037e69e8928
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-16 21:05:08 +0200

    Add files via upload

---
+++ zawartoscWskaznika/zawartoscWskaznika.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned short int myAge = 5, yourAge = 10;
+
+	//wskaźnik
+	unsigned short int* pAge = &myAge;
+
+	cout << "myAge:\t" << myAge << "\t\tyourAge:\t" << yourAge << endl;
+
+	cout << "&myAge:\t" << &myAge << "\t&yourAge:\t" << &yourAge << endl;
+
+	cout << "pAge:\t" << pAge << endl;
+	cout << "*pAge:\t" << *pAge << endl;
+
+	cout << endl << "Ponowne przypisanie: pAge = &yourAge..." << endl << endl;
+	
+	pAge = &yourAge; //ponowne przypisanie do wskaźnika
+
+	cout << "myAge:\t" << myAge << "\t\tyourAge:\t" << yourAge << endl;
+
+	cout << "&myAge:\t" << &myAge << "\t&yourAge:\t" << &yourAge << endl;
+
+	cout << "pAge:\t" << pAge << endl;
+	cout << "*pAge:\t" << *pAge << endl;
+
+	cout << "\n&pAge:\t" << &pAge << endl;
+
+	return 0;
+}
