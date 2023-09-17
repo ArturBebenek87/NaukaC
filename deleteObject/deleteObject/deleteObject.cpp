5a9a0ede8e775c8b0a0af54049fa9371a39d983f
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-17 11:46:41 +0200

    Add files via upload

---
+++ deleteObject/deleteObject/deleteObject.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class SimpleCat
+{
+public:
+	SimpleCat();
+	~SimpleCat();
+private:
+	int itsAge;
+};
+
+SimpleCat::SimpleCat()
+{
+	cout << "Wywołano konstruktor. " << endl;
+	itsAge = 1;
+}
+
+SimpleCat:: ~SimpleCat()
+{
+	cout << "Wywołano destruktor. " << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	cout << "SimpleCat Frisky... " << endl;
+	SimpleCat Frisky;
+	cout << "SimpleCat *pRags = new SimpleCat... " << endl;
+	SimpleCat* pRags = new SimpleCat;
+	cout << "delete pRags... " << endl;
+	delete pRags;
+	cout << "Wyjście, czekaj na Frisky... " << endl;
+
+	return 0;
+}
