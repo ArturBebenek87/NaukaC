de645f08a104d7d6ba2b3b65d1d3990935364311
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-17 11:46:16 +0200

    Add files via upload

---
+++ zwolnienieWskaznika/zwolnienieWskaznika/zwolnienieWskaznika.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int localVeriable = 5;
+	int* pLocal = &localVeriable;
+	int* pHeap = new int;
+	*pHeap = 7;
+	cout << "localVeriable: " << localVeriable << endl;
+	cout << "*pLocal: " << *pLocal << endl;
+	cout << "*pHeap: " << *pHeap << endl;
+	delete pHeap;
+	pHeap = new int;
+	*pHeap = 9;
+	cout << "*pHeap: " << *pHeap << endl;
+	delete pHeap;
+
+	return 0;
+}
