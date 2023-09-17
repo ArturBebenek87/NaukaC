be86d0b3460f2099df5e0539e10161ecc951a4db
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-17 11:47:28 +0200

    Add files via upload

---
+++ referencje/referencje/referencje.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int intOne;
+	int& rSomeRef = intOne;
+
+	intOne = 5;
+	
+	cout << "intOne: " << intOne << endl;
+	cout << "rSomeRef: " << rSomeRef << endl;
+
+	rSomeRef = 7;
+	cout << "intOne: " << intOne << endl;
+	cout << "rSomeRef: " << rSomeRef << endl;
+
+	return 0;
+}
