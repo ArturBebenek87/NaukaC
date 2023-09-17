6b33e96e36ebdb0c4025e68f3618785b501043c3
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-17 11:45:52 +0200

    Add files via upload

---
+++ wskaznikITablice/wskaznikITablice/wskaznikITablice.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int ARRAY_LENGTH = 5;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	//tablica z pięcioma liczbami zainicjalizowana z pięcioma wartościami
+
+	int Numbers[ARRAY_LENGTH] = { 0, 100, 200, 300, 400 };
+
+	//wskazanie na pierwszy element tablicy
+	const int* pInt = Numbers;
+
+	cout << "Użycie wskaźnika w celu wyświetlenia zawartości tablicy: " << endl;
+
+	for (int nIndex = 0; nIndex < ARRAY_LENGTH; ++nIndex)
+		cout << "Element [" << nIndex << "] = " << *(pInt + nIndex) << endl;
+
+	return 0;
+}
