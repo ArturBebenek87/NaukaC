273f5595be5cfb558fcc63450590607360f9066f
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-17 11:47:04 +0200

    Add files via upload

---
+++ wskaznikDoLiczby/wskaznikDoLiczby/wskaznikDoLiczby.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int theInteger;
+	int* pInteger = &theInteger;
+	*pInteger = 5;
+
+	cout << "Liczba całkowita: " << *pInteger << endl;
+
+	return 0;
+}
