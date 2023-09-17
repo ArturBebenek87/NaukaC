26669cc26ade5089a02149160b9073b7028b86d1
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-17 11:47:53 +0200

    Add files via upload

---
+++ adresReferencji/adresReferencji/adresReferencji.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int intOne;
+	int& rSomeRef = intOne;
+
+	intOne = 5;
+
+	cout << "intOne: " << intOne << endl;
+	cout << "rSomeRef: " << rSomeRef << endl << endl;
+
+	cout << "&intOne: " << &intOne << endl;
+	cout << "&rSomeRef: " << &rSomeRef << endl;
+
+	return 0;
+}
