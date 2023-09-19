12b7eb5f9063bf938474e8d02609d1d788ade5b6
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-19 10:48:57 +0200

    Add files via upload

---
+++ przypisanieDoReferencji/przypisanieDoReferencji/przypisanieDoReferencji.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int intOne;
+	int& rSomeRef = intOne;
+
+	intOne = 5;
+
+	cout << "intOne:\t" << intOne << endl;
+	cout << "rSomeRef:\t" << rSomeRef << endl;
+	cout << "&intOne:\t" << &intOne << endl;
+	cout << "&rSomeRef:\t" << &rSomeRef << endl;
+
+	int intTwo = 8;
+
+	rSomeRef = intTwo;
+	cout << endl << "intOne:\t" << intOne << endl;
+	cout << "intTwo:\t" << intTwo << endl;
+	cout << "rSomeRef:\t" << rSomeRef << endl;
+	cout << "&intOne:\t" << &intOne << endl;
+	cout << "&intTwo:\t" << &intTwo << endl;
+	cout << "&rSomeRef:\t" << &rSomeRef << endl;
+
+	return 0;
+}
