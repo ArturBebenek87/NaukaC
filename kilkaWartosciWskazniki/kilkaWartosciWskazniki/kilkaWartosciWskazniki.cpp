c791f0f53177a8a38e5873a3796dfec0fc07043f
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-19 10:50:10 +0200

    Add files via upload

---
+++ kilkaWartosciWskazniki/kilkaWartosciWskazniki/kilkaWartosciWskazniki.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+short Factor(int n, int* pSquared, int* pCubed);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int number, squared, cubed;
+	short error;
+
+	cout << "Wpisz liczbę (0 - 20): ";
+	cin >> number;
+
+	error = Factor(number, &squared, &cubed);
+
+	if (!error)
+	{
+		cout << "liczba: " << number << endl;
+		cout << "do kwadratu: " << squared << endl;
+		cout << "do trzeciej potęgi: " << cubed << endl;
+	}
+	else
+		cout << "Napotkano błąd!!" << endl;
+	
+	return 0;
+}
+
+short Factor(int n, int* pSquared, int* pCubed)
+{
+	short Value = 0;
+	if (n > 20)
+		Value = 1;
+	else
+	{
+		*pSquared = n * n;
+		*pCubed = n * n * n;
+		Value = 0;
+	}
+	return Value;
+}
