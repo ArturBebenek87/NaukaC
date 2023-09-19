a496d25a2d2cd5d6f3cb3e44fd695b68b380d480
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-19 10:49:45 +0200

    Add files via upload

---
+++ swapReferencja/swapReferencja/swapReferencja.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void swap(int& x, int& y);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int x = 5, y = 10;
+
+	cout << "Funkcja main(). Przed funkcją swap(), x: " << x << " y: " << y << endl;
+
+	swap(x, y);
+
+	cout << "Funkcja main(). Po funkcji swap(), x: " << x << " y: " << y << endl;
+	
+	return 0;
+}
+
+void swap(int& rx, int& ry)
+{
+	int temp;
+
+	cout << "Funkcja swap(). Przed zmianą, rx: " << rx << " ry: " << ry << endl;
+
+	temp = rx;
+	rx = ry;
+	ry = temp;
+
+	cout << "Funkcja swap(). Po zmianie, rx: " << rx << " ry: " << ry << endl;
+}
