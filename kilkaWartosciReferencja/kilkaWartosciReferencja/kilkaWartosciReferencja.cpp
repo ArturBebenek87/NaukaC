a46ae6a8dfeac2473117f1063537c3e7b1ad4817
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-22 22:13:42 +0200

    Add files via upload

---
+++ kilkaWartosciReferencja/kilkaWartosciReferencja/kilkaWartosciReferencja.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef unsigned short USHORT;
+enum ERR_CODE {SUCCESS, ERROR};
+
+ERR_CODE Factor(int, int&, int&);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int number, squared, cubed;
+	ERR_CODE result;
+
+	cout << "Wpisz liczbę (0 - 20): ";
+	cin >> number;
+
+	result = Factor(number, squared, cubed);
+
+	if (result == SUCCESS)
+	{
+		cout << "liczba: " << number << endl;
+		cout << "do kwadratu: " << squared << endl;
+		cout << "do trzeciej potęgi: " << cubed << endl;
+	}
+	else
+		cout << "Napotkano błąd!!" << endl;
+	return 0;
+}
+
+ERR_CODE Factor(int n, int& rSquared, int& rCubed)
+{
+	if (n > 20)
+		return ERROR;
+	else
+	{
+		rSquared = n * n;
+		rCubed = n * n * n;
+		return SUCCESS;
+	}
+}
