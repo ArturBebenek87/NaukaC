5fe401b9ebc6c5e8f77c13203b1969082503a694
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-22 22:14:31 +0200

    Add files via upload

---
+++ wskaznikDoObiektuConst/wskaznikDoObiektuConst/wskaznikDoObiektuConst.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class SimpleCat
+{
+public:
+	SimpleCat();
+	SimpleCat(SimpleCat&);
+	~SimpleCat();
+
+	int GetAge() const { return itsAge; }
+	void SetAge(int age) { itsAge = age; }
+
+private:
+	int itsAge;
+};
+
+SimpleCat::SimpleCat()
+{
+	cout << "Konstruktor klasy SimpleCat..." << endl;
+	itsAge = 1;
+}
+
+SimpleCat::SimpleCat(SimpleCat&)
+{
+	cout << "Konstruktor kopiujący klasy SimpleCat..." << endl;
+}
+
+SimpleCat::~SimpleCat()
+{
+	cout << "Destruktor klasy SimpleCat..." << endl;
+}
+
+const SimpleCat* const FunctionTwo
+(const SimpleCat* const theCat);
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	cout << "Tworzę obiekt..." << endl;
+	SimpleCat Frisky;
+	cout << "Frisky ma ";
+	cout << Frisky.GetAge();
+	cout << " lat" << endl;
+	int age = 5;
+	Frisky.SetAge(age);
+	cout << "Frisky ma ";
+	cout << Frisky.GetAge();
+	cout << " lat" << endl;
+	cout << "Wywołuję funkcję FunctionTwo..." << endl;
+	FunctionTwo(&Frisky);
+	cout << "Frisky ma ";
+	cout << Frisky.GetAge();
+	cout << " lat" << endl;
+	return 0;
+}
+
+const SimpleCat* const FunctionTwo
+(const SimpleCat* const theCat)
+{
+	cout << "FunctionTwo. Wracam..." << endl;
+	cout << "Frisky ma teraz " << theCat->GetAge();
+	cout << " lat " << endl;
+
+	return theCat;
+}
