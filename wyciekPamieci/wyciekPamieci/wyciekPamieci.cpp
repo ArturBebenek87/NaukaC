a930f9128d7026825b61882a4839b8c0d60af3a6
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-24 12:44:07 +0200

    Add files via upload

---
+++ wyciekPamieci/wyciekPamieci/wyciekPamieci.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int& GetInt();
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int& rInt = GetInt();
+	cout << "rInt = " << rInt << endl;
+
+	return 0;
+}
+
+int& GetInt()
+{
+	int* pInteger = new int(25);
+
+	return *pInteger;
+}
