e3b0e227d1d773731ee5511ac93cd8d642a38eca
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-09-24 12:43:43 +0200

    Add files via upload

---
+++ referencjaDoNieistniejacegoObiektu/referencjaDoNieistniejacegoObiektu/referencjaDoNieistniejacegoObiektu.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int& GetInt();
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	int& rInt = GetInt();
+	cout << "rInt = " << rInt << endl;
+
+	return 0;
+}
+
+int& GetInt()
+{
+	int nLocalInt = 25;
+
+	return nLocalInt;
+}
