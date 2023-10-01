3d8cbd503a1927a3195cbc4ca39d267387201660
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-01 19:53:50 +0200

    Add files via upload

---
+++ publicznaKlasa/publicznaKlasa/publicznaKlasa.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Cat //deklaracja klasy
+{
+public:
+	int itsAge;
+	int itsWeight;
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	Cat Frisky;
+	Frisky.itsAge = 5;
+
+	cout << "Frisky jest kotem i ma ";
+	cout << Frisky.itsAge << " lat." << endl;
+
+	return 0;
+}
