33f60aab19fa82db9cd61dc65ffa8490b4319a43
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-01 19:54:14 +0200

    Add files via upload

---
+++ prostaKlasa/prostaKlasa/prostaKlasa.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Cat
+{
+public:
+	int GetAge();
+	void SetAge(int age);
+	void Meow();
+private:
+	int itsAge;
+};
+
+int Cat::GetAge()
+{
+	return itsAge;
+}
+
+void Cat::SetAge(int age)
+{
+	itsAge = age;
+}
+
+void Cat::Meow()
+{
+	cout << "Miauczy." << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	Cat Frisky;
+	Frisky.SetAge(5);
+	Frisky.Meow();
+
+	cout << "Frisky jest kotem i ma ";
+	cout << Frisky.GetAge() << " lat." << endl;
+	Frisky.Meow();
+
+	return 0;
+}
