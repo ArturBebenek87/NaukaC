282aa328066d2d6913f83af942b1e2a0953f04dd
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-03 21:40:58 +0200

    Add files via upload

---
+++ konstruktorDestruktor/konstruktorDestruktor/konstruktorDestruktor.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Cat
+{
+public:
+	Cat(int initialAge);
+	~Cat();
+	int GetAge();
+	void SetAge(int age);
+	void Meow();
+private:
+	int itsAge;
+};
+
+Cat::Cat(int initialAge)
+{
+	itsAge = initialAge;
+}
+
+Cat::~Cat()
+{
+}
+
+int Cat::GetAge()
+{
+	return itsAge;
+}
+
+void Cat::SetAge(int age)
+{
+	itsAge = age;
+}
+
+void Cat::Meow()
+{
+	cout << "Miauczy." << endl;
+}
+
+int main()
+{
+	Cat Frisky(5);
+	Frisky.Meow();
+	cout << "Frisky jest kotem i ma ";
+	cout << Frisky.GetAge() << " lat." << endl;
+	Frisky.Meow();
+	Frisky.SetAge(7);
+	cout << "Teraz Frisky ma ";
+	cout << Frisky.GetAge() << " lat." << endl;
+
+	return 0;
+}
