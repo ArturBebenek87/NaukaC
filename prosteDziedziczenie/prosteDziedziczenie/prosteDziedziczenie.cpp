7f9248d81cf848b22f1617e75e0a75140bc03953
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-04 22:23:08 +0200

    Add files via upload

---
+++ prosteDziedziczenie/prosteDziedziczenie/prosteDziedziczenie.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum BREED {GOLDEN, CAIRN, DANDIE, SHETLAND, DOBERMAN, LAB};
+
+class Mammal
+{
+public:
+	Mammal();
+	~Mammal();
+
+	int GetAge() const;
+	void SetAge(int);
+	int GetWeight() const;
+	void SetWeight();
+
+	void Speak() const;
+	void Sleep() const;
+
+protected:
+	int itsAge;
+	int itsWeight;
+};
+
+class Dog : public Mammal
+{
+public:
+	Dog();
+	~Dog();
+
+	BREED GetBreed() const;
+	void SetBreed(BREED);
+
+	void WagTail();
+	void BegForFood();
+
+protected:
+	BREED itsBreed;
+};
