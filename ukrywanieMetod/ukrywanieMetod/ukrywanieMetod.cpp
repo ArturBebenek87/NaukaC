b1443d44bf1e000c14f85d3fde8b705028b4e708
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-09 22:55:05 +0200

    Add files via upload

---
+++ ukrywanieMetod/ukrywanieMetod/ukrywanieMetod.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Mammal
+{
+public:
+	void Move() const { cout << "Ssak przeszedł jeden krok.\n"; }
+	void Move(int distance) const
+	{
+		cout << "Ssak przeszedł ";
+		cout << distance << " kroki.\n";
+	}
+protected:
+	int itsAge;
+	int itsWeight;
+};
+
+class Dog : public Mammal
+{
+public:
+	void Move() const { cout << "Pies przeszedł 5 kroków.\n"; }
+};
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	Mammal bigAnimal;
+	Dog Fido;
+	bigAnimal.Move();
+	bigAnimal.Move(2);
+	Fido.Move();
+	return 0;
+}
