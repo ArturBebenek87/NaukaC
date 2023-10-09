0051147c3e609d2884da51989862ac42310f38b2
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-09 22:54:39 +0200

    Add files via upload

---
+++ przesloniecieMetodKlasy/przesloniecieMetodKlasy/przesloniecieMetodKlasy.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum BREED {GOLDEN, CAIRN, DANDIE, SHETLAND, DOBERMAN, LAB};
+
+class Mammal
+{
+public:
+	Mammal() { cout << "Konstruktor klasy Mammal...\n"; }
+	~Mammal() { cout << "Destruktor klasy Mammal...\n"; }
+
+	void Speak() const { cout << "Dźwięk ssaka!\n"; }
+	void Sleep() const { cout << "Cicho. Właśnie śpię.\n"; }
+
+protected:
+	int itsAge;
+	int itsWeight;
+};
+
+class Dog : public Mammal
+{
+public:
+	Dog() { cout << "Konstruktor klasy Dog...\n"; }
+	~Dog() { cout << "Destruktor klasy Dog...\n"; }
+
+	void WagTail() const { cout << "Macham ogonkiem...\n"; }
+	void BegForFood() const { cout << "Proszę o jedzenie...\n"; }
+	void Speak() const { cout << "Hau!\n"; }
+
+private:
+	BREED itsBreed;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	Mammal bigAnimal;
+	Dog Fido;
+	bigAnimal.Speak();
+	Fido.Speak();
+	return 0;
+}
