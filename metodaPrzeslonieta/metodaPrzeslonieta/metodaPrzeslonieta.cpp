9cd6a8965b628146e366214c0ee10b89647abf78
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-11 20:02:58 +0200

    Add files via upload

---
+++ metodaPrzeslonieta/metodaPrzeslonieta/metodaPrzeslonieta.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Mammal
+{
+public:
+	void Move() const { cout << "Ssak przeszedł jeden krok.\n"; }
+	void Move(int distance) const
+	{
+		cout << "Ssak przeszedł " << distance;
+		cout << " kroki." << endl;
+	}
+
+protected:
+	int itsAge;
+	int itsWeight;
+};
+
+class Dog : public Mammal
+{
+public:
+	void Move() const;
+};
+
+void Dog::Move() const
+{
+	cout << "W metodzie Move klasy Dog...\n";
+	Mammal::Move(3);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	Mammal bigAnimal;
+	Dog Fido;
+	bigAnimal.Move(2);
+	Fido.Mammal::Move(6);
+	return 0;
+}
