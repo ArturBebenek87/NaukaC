396c7c94aa70efcbb485eb6c91d18b0940bab641
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-21 22:31:03 +0200

    Add files via upload

---
+++ wieleFunkcjiWirtualnych/wieleFunkcjiWirtualnych/wieleFunkcjiWirtualnych.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Mammal
+{
+public:
+	Mammal(): itsAge(1){ }
+	virtual ~Mammal() { }
+	virtual void Speak() const { cout << "Ssak mów!\n"; }
+protected:
+	int itsAge;
+};
+
+class Dog : public Mammal
+{
+public:
+	void Speak() const { cout << "Hau!\n"; }
+};
+
+class Cat : public Mammal
+{
+public:
+	void Speak() const { cout << "Miau!\n"; }
+};
+
+class Horse : public Mammal
+{
+public:
+	void Speak() const { cout << "Ihaa!\n"; }
+};
+
+class Pig : public Mammal
+{
+public:
+	void Speak() const { cout << "Kwik!\n"; }
+};
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	Mammal* theArray[5];
+	Mammal* ptr;
+	int choice, i;
+
+	for (i = 0; i < 5; i++)
+	{
+		cout << "(1)pies (2)kot (3)koń (4)świnia: ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1: ptr = new Dog;
+			break;
+		case 2: ptr = new Cat;
+			break;
+		case 3: ptr = new Horse;
+			break;
+		case 4: ptr = new Pig;
+			break;
+		default: ptr = new Mammal;
+			break;
+		}
+		theArray[i] = ptr;
+	}
+	for (i = 0; i < 5; i++)
+		theArray[i]->Speak();
+	return 0;
+}
