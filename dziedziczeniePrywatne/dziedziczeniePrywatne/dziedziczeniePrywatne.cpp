ce14260b0fc682c5d8b6c4cb81e2bbd90fb0b610
Artur <112261978+ArturBebenek87@users.noreply.github.com>
2023-10-22 22:11:25 +0200

    Add files via upload

---
+++ dziedziczeniePrywatne/dziedziczeniePrywatne/dziedziczeniePrywatne.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class ElecticMotor
+{
+public:
+	ElecticMotor() {};
+	virtual ~ElecticMotor() {};
+
+public:
+	void StartMotor()
+	{
+		Accelerate();
+		Cruise();
+	}
+
+	void StopMotor()
+	{
+		cout << "Silnik zatrzymany" << endl;
+	}
+
+private:
+	void Accelerate()
+	{
+		cout << "Silnik uruchomiony" << endl;
+	}
+
+	void Cruise()
+	{
+		cout << "Silnik działa ze stałą prędkością" << endl;
+	}
+};
+
+class Fan : private ElecticMotor
+{
+public:
+	Fan() {};
+	~Fan() {};
+
+	void StartFan()
+	{
+		StartMotor();
+	}
+
+	void StopFan()
+	{
+		StopMotor();
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "PL_pl");
+
+	Fan mFan;
+
+	mFan.StartFan();
+	mFan.StopFan();
+
+	return 0;
+}
